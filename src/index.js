8dcdca85e2ca7302458df42154ec50a00d252270
Gary Hall <guategary@gmail.com>
2018-05-10 23:33:01 -0400

    Use npm pkg for menu control

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import gotoMenu from './gotoMenu';
-
-// eslint-disable-next-line import/no-extraneous-dependencies
 require('normalize.css');
 
-// normalize.css must be loaded first before app css, so disable eslint check
-/* eslint-disable import/first */
-import './assets/css/style.scss';
-/* eslint-enable import/first */
+require('goto-dropdown/lib/goto-style.css');
 
-gotoMenu.navControl('click');
+const goto = require('goto-dropdown');
+
+goto.navControl('click');
--- webpack.common.js
+++ webpack.common.js
@@ -21,6 +21,9 @@ module.exports = {
 	   }
      ]
    },
+   resolve: {
+     symlinks: false
+   },
    plugins: [
 	 ExtractSass
    ]
